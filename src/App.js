6478acc401d1f7efc8e85d8ec638aa32c8717939
MonMihle <145695872+MonMihle@users.noreply.github.com>
2024-04-18 18:06:00 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ function App() {
   return (
     <div className="App">
       <h1>Hey, it's me!</h1>
+      <footer>
+        This project was coded by Monica Mntanywa and is {""}
+        <a href="https://github.com/MonMihle/weather-react-app">
+          open source on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
