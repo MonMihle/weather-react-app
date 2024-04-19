e7916baa7b0a6b99ab822cf75127938c825e2f5c
MonMihle <145695872+MonMihle@users.noreply.github.com>
2024-04-19 22:25:28 +0200

    API intergration

--- src/Weather.js
+++ src/Weather.js
@@ -5,19 +5,16 @@ import "./Weather.css";
 export default function Weather() {
   const [ready, setReady] = useState(false);
   const [temperature, setTemperature] = useState(null);
-  //const [weatherData, setWeatherData] = useState({});
+
   function handleResponse(response) {
     console.log(response.data);
-    set weatherData({temperature: response.data.main.temperature,
-    wind:});
+    setTemperature(response.data.main.temp);
     setReady(true);
   }
 
-  if (ready) 
-  {
+  if (ready) {
     return (
       <div className="Weather">
-        <br />
         <form>
           <div className="row">
             <div className="col-9">
@@ -29,7 +26,11 @@ export default function Weather() {
               />
             </div>
             <div className="col-3">
-              <input type="submit" value="Search" className="btn btn-primary w-100" />
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
         </form>
@@ -41,16 +42,16 @@ export default function Weather() {
         <div className="row mt-3">
           <div className="col-6">
             <div className="clearfix">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-              alt="Clear"
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+                alt="Clear"
+                className="float-left"
+              />
               <div className="float-left">
-            />
-            <div className = "float-left">
                 <span className="temperature">{Math.round(temperature)}</span>
                 <span className="unit">°C</span>
-          </div>
-          </div>
+              </div>
+            </div>
           </div>
           <div className="col-6">
             <ul>
@@ -62,14 +63,11 @@ export default function Weather() {
         </div>
       </div>
     );
-    }
-
-  else {
+  } else {
     const apiKey = "c11eb907238f0b8255b8ad494906f796";
-    let city="Cape Town"
+    let city = "Cape Town";
     let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiURL).then(handleResponse);
-    return "Loading"
+    return "Loading...";
   }
-    } 
-
+}
